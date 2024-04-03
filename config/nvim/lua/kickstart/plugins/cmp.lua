93ee201a5e29965fa508acb8bc7b9dcfad77c642
Gaweringo <40121865+Gaweringo@users.noreply.github.com>
2024-04-03 13:06:02 +0200

    nvim: update cmp keybindings

--- config/nvim/lua/kickstart/plugins/cmp.lua
+++ config/nvim/lua/kickstart/plugins/cmp.lua
@@ -54,15 +54,15 @@ return {
           -- Select the [p]revious item
           ['<C-p>'] = cmp.mapping.select_prev_item(),
 
-          -- Accept ([y]es) the completion.
+          -- Accept ([i]nsert) the completion.
           --  This will auto-import if your LSP supports it.
           --  This will expand snippets if the LSP sent a snippet.
-          ['<C-y>'] = cmp.mapping.confirm { select = true },
+          ['<C-i>'] = cmp.mapping.confirm { select = true },
 
           -- Manually trigger a completion from nvim-cmp.
           --  Generally you don't need this, because nvim-cmp will display
           --  completions whenever it has completion options available.
-          ['<C-Space>'] = cmp.mapping.complete {},
+          ['<C-s>'] = cmp.mapping.complete {},
 
           -- Think of <c-l> as moving to the right of your snippet expansion.
           --  So if you have a snippet that's like:
