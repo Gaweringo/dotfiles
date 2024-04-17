5ac247c27b65dc9ef6b1f72647a7c7e4356dcf1a
Gaweringo <40121865+Gaweringo@users.noreply.github.com>
2024-04-17 15:39:09 +0200

    nvim: add json formatter

--- config/nvim/lua/kickstart/plugins/conform.lua
+++ config/nvim/lua/kickstart/plugins/conform.lua
@@ -10,6 +10,7 @@ return {
       formatters_by_ft = {
         lua = { 'stylua' },
         vhdl = { 'hdl_emacs' },
+        json = { 'fixjson' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
