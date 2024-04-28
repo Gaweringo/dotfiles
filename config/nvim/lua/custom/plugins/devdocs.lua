65c8dc81d0c7babf14af7ded42c9704e68f091fc
Gaweringo <40121865+Gaweringo@users.noreply.github.com>
2024-04-28 21:31:56 +0200

    nvim: add devdocs

---
+++ config/nvim/lua/custom/plugins/devdocs.lua
@@ -0,0 +1,20 @@
+return {
+  'luckasRanarison/nvim-devdocs',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-telescope/telescope.nvim',
+    'nvim-treesitter/nvim-treesitter',
+  },
+  opts = {
+    wrap = true,
+    previewer_cmd = 'glow',
+    cmd_args = { '-s', 'auto', '-w', '97' },
+    cmd_ignore = {},
+    picker_cmd = true,
+    picker_cmd_args = { '-s', 'auto', '-w', '45' },
+    mappings = {
+      open_in_browser = '<leader>vb',
+      toggle_rendering = '<leader>vr',
+    },
+  },
+}
