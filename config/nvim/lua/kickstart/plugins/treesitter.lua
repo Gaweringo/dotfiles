35cd67ae263913d42dcfc29202e5eed6f6fbd3a8
Gaweringo <40121865+Gaweringo@users.noreply.github.com>
2024-05-04 00:57:58 +0200

    nvim: more lazy loading (treesitter)

--- config/nvim/lua/kickstart/plugins/treesitter.lua
+++ config/nvim/lua/kickstart/plugins/treesitter.lua
@@ -2,6 +2,7 @@ return {
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
+    event = 'VeryLazy',
     config = function()
       -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
 
