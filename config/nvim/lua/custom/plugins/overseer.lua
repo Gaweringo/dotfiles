9138a80b0b04e4b6dda60cda36b5f1f707294dc6
Gaweringo <40121865+Gaweringo@users.noreply.github.com>
2024-05-04 23:56:10 +0200

    nvim: add overseer itself

---
+++ config/nvim/lua/custom/plugins/overseer.lua
@@ -0,0 +1,8 @@
+return {
+  'stevearc/overseer.nvim',
+  keys = {
+    { '<leader>po', '<cmd>OverseerRun<cr>', desc = '[o]verseer run' },
+    { '<leader>pO', '<cmd>OverseerToggle<cr>', desc = '[O]verseer toggle' },
+  },
+  opts = {},
+}
