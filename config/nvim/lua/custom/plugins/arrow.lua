483e71b42d6eca2988bd23dd8775575c6cb31b03
Gaweringo <40121865+Gaweringo@users.noreply.github.com>
2024-05-05 20:24:33 +0200

    nvim: update arrow keymap

--- config/nvim/lua/custom/plugins/arrow.lua
+++ config/nvim/lua/custom/plugins/arrow.lua
@@ -1,9 +1,40 @@
 return {
   'otavioschwanck/arrow.nvim',
+  event = 'VeryLazy',
   opts = {
     show_icons = true,
-    leader_key = ';', -- Recommended to be a single key
+    leader_key = 'ö', -- Recommended to be a single key
     buffer_leader_key = 'm', -- Per Buffer Mappings
     relative_path = true,
+  },
+  keys = {
+    {
+      '<A-h>',
+      function()
+        require('arrow.persist').go_to(1)
+      end,
+      'Arrow first',
+    },
+    {
+      '<A-j>',
+      function()
+        require('arrow.persist').go_to(2)
+      end,
+      'Arrow first',
+    },
+    {
+      '<A-k>',
+      function()
+        require('arrow.persist').go_to(3)
+      end,
+      'Arrow first',
+    },
+    {
+      '<A-l>',
+      function()
+        require('arrow.persist').go_to(3)
+      end,
+      'Arrow first',
+    },
   },
 }
