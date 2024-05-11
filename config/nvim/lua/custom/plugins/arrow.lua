5af4a996179a6191fc3e4a368e46ac81102fab51
Gaweringo <40121865+Gaweringo@users.noreply.github.com>
2024-05-11 16:18:19 +0200

    nvim: fix some things in arrow and comment

--- config/nvim/lua/custom/plugins/arrow.lua
+++ config/nvim/lua/custom/plugins/arrow.lua
@@ -5,7 +5,6 @@ return {
     show_icons = true,
     leader_key = 'ö', -- Recommended to be a single key
     buffer_leader_key = 'm', -- Per Buffer Mappings
-    relative_path = true,
   },
   keys = {
     {
