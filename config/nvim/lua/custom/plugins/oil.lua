7797afb02369989c8e07efa81c58357f91a2039c
Gaweringo <40121865+Gaweringo@users.noreply.github.com>
2024-05-16 15:37:50 +0200

    nvim: add oil.nvim

---
+++ config/nvim/lua/custom/plugins/oil.lua
@@ -0,0 +1,13 @@
+return {
+  'stevearc/oil.nvim',
+  opts = {
+    columns = {
+      'icon',
+      -- "permissions",
+      "size",
+      "mtime",
+    },
+  },
+  -- Optional dependencies
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
+}
