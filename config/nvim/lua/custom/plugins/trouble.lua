b1ce510964e9a9bd2be7b567d630b96802ed0293
Gaweringo <40121865+Gaweringo@users.noreply.github.com>
2024-06-11 19:46:07 +0200

    nvim: make trouble next/prev repeatable with , and ;

--- config/nvim/lua/custom/plugins/trouble.lua
+++ config/nvim/lua/custom/plugins/trouble.lua
@@ -1,3 +1,27 @@
+local trouble_next = function()
+  if require('trouble').is_open() then
+    require('trouble').next { skip_groups = true, jump = true }
+  else
+    local ok, err = pcall(vim.cmd.cnext)
+    if not ok then
+      vim.notify(err, vim.log.levels.ERROR)
+    end
+  end
+end
+local trouble_previous = function()
+  if require('trouble').is_open() then
+    require('trouble').prev { skip_groups = true, jump = true }
+  else
+    local ok, err = pcall(vim.cmd.cprev)
+    if not ok then
+      vim.notify(err, vim.log.levels.ERROR)
+    end
+  end
+end
+
+local ts_repeat_move = require 'nvim-treesitter.textobjects.repeatable_move'
+local trouble_next_repeat, trouble_previous_repeat = ts_repeat_move.make_repeatable_move_pair(trouble_next, trouble_previous)
+
 return {
   'folke/trouble.nvim',
   keys = {
@@ -41,34 +65,8 @@ return {
       '<cmd>Trouble qflist toggle<cr>',
       desc = 'Quickfix List (Trouble)',
     },
-    {
-      '[q',
-      function()
-        if require('trouble').is_open() then
-          require('trouble').prev { skip_groups = true, jump = true }
-        else
-          local ok, err = pcall(vim.cmd.cprev)
-          if not ok then
-            vim.notify(err, vim.log.levels.ERROR)
-          end
-        end
-      end,
-      desc = 'Previous Trouble/Quickfix Item',
-    },
-    {
-      ']q',
-      function()
-        if require('trouble').is_open() then
-          require('trouble').next { skip_groups = true, jump = true }
-        else
-          local ok, err = pcall(vim.cmd.cnext)
-          if not ok then
-            vim.notify(err, vim.log.levels.ERROR)
-          end
-        end
-      end,
-      desc = 'Next Trouble/Quickfix Item',
-    },
+    { '[q', trouble_previous_repeat, desc = 'Previous Trouble/Quickfix Item' },
+    { ']q', trouble_next_repeat, desc = 'Next Trouble/Quickfix Item' },
   },
   opts = {},
 }
