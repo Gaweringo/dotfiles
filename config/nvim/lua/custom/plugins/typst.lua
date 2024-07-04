fbdc5a81e1abd03f569fdc0243f2f519907d8704
Gaweringo <40121865+Gaweringo@users.noreply.github.com>
2024-07-04 10:16:36 +0200

    fix(nvim/typst): update typst preview on save instead of on type

--- config/nvim/lua/custom/plugins/typst.lua
+++ config/nvim/lua/custom/plugins/typst.lua
@@ -5,7 +5,7 @@ return {
   config = function()
     require('lspconfig').typst_lsp.setup {
       settings = {
-        exportPdf = 'onType', -- Choose onType, onSave or never.
+        exportPdf = 'onSave', -- Choose onType, onSave or never.
         -- serverPath = "" -- Normally, there is no need to uncomment it.
       },
     }
