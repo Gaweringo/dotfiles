52a200dcf5779d3cc9cb006f819c45360d2de056
Gaweringo <40121865+Gaweringo@users.noreply.github.com>
2024-09-25 02:05:48 +0200

    feat(nvim): add ability to toggle format on save on and off

--- config/nvim/lua/kickstart/plugins/conform.lua
+++ config/nvim/lua/kickstart/plugins/conform.lua
@@ -1,12 +1,18 @@
 return {
   { -- Autoformat
     'stevearc/conform.nvim',
+
+    -- @module "conform"
+    -- @type conform.setupOpts
     opts = {
       notify_on_error = false,
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_fallback = true,
-      },
+      format_on_save = function(bufnr)
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return
+        end
+        return { timeout_ms = 500, lsp_format = 'fallback' }
+      end,
       formatters_by_ft = {
         lua = { 'stylua' },
         vhdl = { 'hdl_emacs' },
@@ -35,19 +41,60 @@ return {
     keys = {
       { '<leader>F', '<cmd>Format<CR>', desc = 'Format document' },
     },
-  },
-  vim.api.nvim_create_user_command('Format', function(args)
-    local range = nil
-    if args.count ~= -1 then
-      local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
-      range = {
-        start = { args.line1, 0 },
-        ['end'] = { args.line2, end_line:len() },
+    cmd = { 'ConformInfo', 'Format' },
+    config = function(plugin)
+      require('conform').setup(plugin.opts)
+
+      -- FormatCommand
+      vim.api.nvim_create_user_command('Format', function(args)
+        local range = nil
+        if args.count ~= -1 then
+          local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+          range = {
+            start = { args.line1, 0 },
+            ['end'] = { args.line2, end_line:len() },
+          }
+        end
+        require('conform').format { async = true, lsp_fallback = true, range = range }
+      end, { range = true })
+      vim.keymap.set('n', '<leader>F', ':Format<CR>', { desc = 'Format document' })
+
+      -- Format on save toggles
+      vim.api.nvim_create_user_command('FormatDisable', function(args)
+        if args.bang then
+          -- FormatDisable! will disable formatting just for this buffer
+          vim.b.disable_autoformat = true
+        else
+          vim.g.disable_autoformat = true
+        end
+      end, {
+        desc = 'Disable autoformat-on-save',
+        bang = true,
+      })
+
+      vim.api.nvim_create_user_command('FormatEnable', function()
+        vim.b.disable_autoformat = false
+        vim.g.disable_autoformat = false
+      end, {
+        desc = 'Re-enable autoformat-on-save',
+      })
+
+      vim.api.nvim_create_user_command('FormatToggle', function()
+        vim.b.disable_autoformat = not vim.b.disable_autoformat
+        vim.g.disable_autoformat = not vim.g.disable_autoformat
+      end, {
+        desc = 'Toggles autoformat-on-save',
+      })
+
+      require('which-key').add {
+        '<leader>tF',
+        '<cmd>FormatToggle<CR>',
+        desc = function()
+          return '[t]urn [F]ormat on save -> ' .. (vim.g.disable_autoformat and 'ON' or 'OFF')
+        end,
       }
-    end
-    require('conform').format { async = true, lsp_fallback = true, range = range }
-  end, { range = true }),
-  vim.keymap.set('n', '<leader>F', ':Format<CR>', { desc = 'Format document' }),
+    end,
+  },
 }
 
 -- vim: ts=2 sts=2 sw=2 et
