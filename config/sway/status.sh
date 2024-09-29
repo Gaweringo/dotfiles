0ee28cb1135953380c25fb434ded2945c74e6c9b
Gaweringo <40121865+Gaweringo@users.noreply.github.com>
2024-09-29 22:19:22 +0200

    feat(sway): add more metrics to swaybar

--- config/sway/status.sh
+++ config/sway/status.sh
@@ -5,9 +5,8 @@
 # Produces "21 days", for example
 uptime_formatted=$(uptime | cut -d ',' -f1  | cut -d ' ' -f4,5)
 
-# The abbreviated weekday (e.g., "Sat"), followed by the ISO-formatted date
-# like 2018-10-06 and the time (e.g., 14:01)
-date_formatted=$(date "+%a %F %H:%M")
+# current date formatted as 'Sun 2024-19-29 09:59:08 PM'
+date_formatted=$(date +'%a %Y-%m-%d %X';)
 
 # Get the Linux version but remove the "-1-ARCH" part
 linux_version=$(uname -r | cut -d '-' -f1)
@@ -15,10 +14,19 @@ linux_version=$(uname -r | cut -d '-' -f1)
 # current keyboard layout
 current_layout=$(swaymsg -t get_inputs | jq 'map(select(has("xkb_active_layout_name")))[0].xkb_active_layout_name')
 
-sep="   |   "
+# current disk usage
+disk_usage=$(df -h / | awk 'NR==2 {print $5}')
+
+# current cpu usage
+cpu_usage=$(top -bn1 | grep "Cpu(s)" | awk '{printf "%.1f", $2 + $4}')
+
+# current ram usage
+ram_usage=$(top -bn1 | grep "B Mem" | awk '{printf "%.1f", $8/$4 * 100.0}')
+
+sep=" | "
 
 # Returns the battery status: "Full", "Discharging", or "Charging".
 # battery_status=$(cat /sys/class/power_supply/BAT0/status)
 
 # Emojis and characters for the status bar
-echo   '  ' $current_layout $sep ↑ $uptime_formatted $sep  $linux_version $sep $date_formatted
+echo   '  ' $current_layout "$sep"   '  ' $ram_usage% "$sep"   ' ' $cpu_usage% "$sep"   ' ' $disk_usage "$sep" ↑ $uptime_formatted "$sep"  ' ' $linux_version "$sep" $date_formatted
