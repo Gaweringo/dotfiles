d5987314899b1d84e8858852c5eca5ddf6f1c6de
Gaweringo <40121865+Gaweringo@users.noreply.github.com>
2024-10-06 14:50:46 +0200

    feat(nvim): add markdown extensions

---
+++ config/nvim/lua/custom/plugins/markdown.lua
@@ -0,0 +1,23 @@
+return {
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    ft = 'markdown',
+    cmd = 'RenderMarkdown',
+    opts = {},
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+  },
+
+  -- install without yarn or npm
+  {
+    'iamcco/markdown-preview.nvim',
+    cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
+    ft = { 'markdown' },
+    build = function()
+      require('lazy').load { plugins = { 'markdown-preview.nvim' } }
+      vim.fn['mkdp#util#install']()
+    end,
+    init = function()
+      vim.g.mkdp_browser = 'chromium'
+    end,
+  },
+}
