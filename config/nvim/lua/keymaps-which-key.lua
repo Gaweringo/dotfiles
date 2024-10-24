4a2b4322ce3e4fd54cc024c3a01e8e84db20d79c
Gaweringo <40121865+Gaweringo@users.noreply.github.com>
2024-10-24 13:31:26 +0200

    feat(nvim): add toggle background and toggle textwdith

--- config/nvim/lua/keymaps-which-key.lua
+++ config/nvim/lua/keymaps-which-key.lua
@@ -1,5 +1,21 @@
 local wk = require 'which-key'
 
+local new_bg_fn = function()
+  local new_bg = 'dark'
+  if vim.o.background == 'dark' then
+    new_bg = 'light'
+  end
+  return new_bg
+end
+
+local new_tw_fn = function()
+  local new_tw = 90
+  if vim.o.textwidth ~= 0 then
+    new_tw = 0
+  end
+  return new_tw
+end
+
 -- Toggles
 wk.add {
   {
@@ -25,6 +41,25 @@ wk.add {
         new_lang = 'de'
       end
       return '[t]urn [L]anguage spelllang ' .. cur_lang .. ' -> ' .. new_lang
+    end,
+  },
+  {
+    '<leader>tb',
+    function()
+      vim.o.background = new_bg_fn()
+    end,
+    desc = function()
+      return '[t]oggle [b]ackground -> ' .. new_bg_fn()
+    end,
+  },
+  {
+    '<leader>tw',
+    function()
+      vim.o.textwidth = new_tw_fn()
+      vim.o.colorcolumn = '+1'
+    end,
+    desc = function()
+      return '[t]oggle text[w]idth -> ' .. new_tw_fn()
     end,
   },
 }
