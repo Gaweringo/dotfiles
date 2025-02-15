91a3c2940d4dc71e5d94a31084ae17e2a10b6b06
Gaweringo <40121865+Gaweringo@users.noreply.github.com>
2025-02-15 14:39:50 +0100

    feat(nvim/snippets): add qty snippet for typst

---
+++ config/nvim/lua/kickstart/plugins/snippets/typst.lua
@@ -0,0 +1,35 @@
+-- This clears snippets, so when I source this file
+-- I can try the snippets again, without restarting neovim.
+--
+-- This is pretty useful if you're trying to do something a bit
+-- more complicated or just exploring random snippet ideas
+require('luasnip.session.snippet_collection').clear_snippets 'typst'
+
+local ls = require 'luasnip'
+
+local fmta = require('luasnip.extras.fmt').fmta
+local rep = require('luasnip.extras').rep
+
+local s = ls.snippet
+local c = ls.choice_node
+local d = ls.dynamic_node
+local i = ls.insert_node
+local t = ls.text_node
+local sn = ls.snippet_node
+
+ls.add_snippets('typst', {
+  s(
+    'qty',
+    fmta(
+      [[
+qty("<num>", "<unit>")<finish>
+]],
+      {
+        num = i(1),
+        unit = i(2),
+        finish = i(0),
+      }
+    )
+  ),
+})
+print(string.match(vim.fn.expand '%:t:r', '^[^-]+'))
