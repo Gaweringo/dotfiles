1796f2b1e5893c8ac8d0089413f0ff5a2e5a84f7
Gaweringo <40121865+Gaweringo@users.noreply.github.com>
2025-09-14 18:53:20 +0200

    feat(nvim): remove bmessages

--- config/nvim/lua/plugins/02_no-config.lua
+++ config/nvim/lua/plugins/02_no-config.lua
@@ -3,14 +3,6 @@ return {
 
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {}, event = { 'VeryLazy', 'BufReadPre', 'BufNewFile' } },
-
-  {
-    -- Open messages in a live updating buffer
-    'ariel-frischer/bmessages.nvim',
-    commit = '985c6973bc27888598a03f4766a2481248a599b2',
-    opts = {},
-    cmd = { 'Bmessages', 'Bmessagesvs', 'Bmessagessp', 'BmessagesEdit' },
-  },
   {
     enabled = false,
     "nvzone/showkeys",
