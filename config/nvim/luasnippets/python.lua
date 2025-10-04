391fdce8043ce082295e0c950254d4f34eab1516
Gaweringo <40121865+Gaweringo@users.noreply.github.com>
2025-10-04 19:25:19 +0200

    feat(nvim/snippets/python): add import snippets for common python libs

---
+++ config/nvim/luasnippets/python.lua
@@ -0,0 +1,6 @@
+return {}, {
+  s('imnp', { t 'import numpy as np' }),
+  s('implt', { t 'import matplotlib.pyplot as plt' }),
+  s('imall', { t { 'import numpy as np', 'import matplotlib.pyplot as plt' } }),
+  s('imPath', { t 'from pathlib import Path' }),
+}
