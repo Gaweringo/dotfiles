4ad1abec37d426d7735b66e104b1cf1f3fdcf9f5
Gaweringo <40121865+Gaweringo@users.noreply.github.com>
2025-10-05 02:43:08 +0200

    feat(nvim): for nvim-pqf to not trim lines in output

--- config/nvim/lua/plugins/nvim-pqf.lua
+++ config/nvim/lua/plugins/nvim-pqf.lua
@@ -1,7 +1,9 @@
 return {
   -- TODO: Maybe switch to quicker.nvim or trouble.nvim
-  'yorickpeterse/nvim-pqf',
+  -- 'yorickpeterse/nvim-pqf',
+  'gaweringo/nvim-pqf',
   opts = {
+    max_filename_length = 80,
     signs = {
       error = { text = '✘', hl = 'DiagnosticSignError' },
       warning = { text = ' ', hl = 'DiagnosticSignWarn' },
