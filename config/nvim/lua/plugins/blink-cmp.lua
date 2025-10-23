6d2b71f94d0af8a98aab61af843193dadbae40a8
Gaweringo <40121865+Gaweringo@users.noreply.github.com>
2025-10-23 08:49:16 +0200

    feat(nvim/blink): add omni as source for blink autocomplete

--- config/nvim/lua/plugins/blink-cmp.lua
+++ config/nvim/lua/plugins/blink-cmp.lua
@@ -61,7 +61,7 @@ return {
     -- Default list of enabled providers defined so that you can extend it
     -- elsewhere in your config, without redefining it, due to `opts_extend`
     sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer', 'lazydev', 'git' },
+      default = { 'lsp', 'path', 'snippets', 'buffer', 'lazydev', 'git', 'omni' },
       -- TODO: Look for some providers which might be nice: https://cmp.saghen.dev/configuration/sources.html#community-sources
       providers = {
         git = {
