295a0065b554217a403fbd0cd127a71116cb705a
Gaweringo <40121865+Gaweringo@users.noreply.github.com>
2025-11-16 02:23:27 +0100

    feat(nvim/luasnip): cpp snippet for rule of five

---
+++ config/nvim/luasnippets/cpp.lua
@@ -0,0 +1,44 @@
+local ts_utils = require('nvim-treesitter.ts_utils')
+
+return { -- Normal snippets
+  s({ trig = 'rfive', desc = 'Implement rule of five' },
+    fmta([[
+virtual ~<class_name>()<action>;
+<class_name>(<class_name>&&)<action>;
+<class_name>& operator=(<class_name>&&)<action>;
+<class_name>(const <class_name>&)<action>;
+<class_name>& operator=(const <class_name>&)<action>;
+<finish>
+      ]],
+      {
+        class_name = d(1, function()
+          -- Find name of current class or struct
+          local status, node = pcall(ts_utils.get_node_at_cursor)
+          while status and node ~= nil do
+            if node:type() == "struct_specifier" or node:type() == "class_specifier" then
+              local name = node:field('name')
+              if name == nil then break end
+              name = name[1]
+              if name == nil then break end
+              name = vim.treesitter.get_node_text(name, 0)
+              return sn(nil, { i(1, name) })
+            end
+            node = node:parent()
+          end
+
+          return sn(nil, { i(1, "ClassName") })
+        end),
+        action = c(2,
+          {
+            i(1, " = delete"),
+            t "",
+          }
+        ),
+        finish = i(0),
+      },
+      { repeat_duplicates = true }
+    )
+  )
+}, { -- Autosnippets
+}
+
