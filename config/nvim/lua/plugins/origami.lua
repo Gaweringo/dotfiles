816dd09ce14f11c0f6b86098fd03c00d4c8c38e3
Gaweringo <40121865+Gaweringo@users.noreply.github.com>
2025-12-21 11:58:11 +0100

    feat(nvim/origami): don't use h and l for fold open/close

--- config/nvim/lua/plugins/origami.lua
+++ config/nvim/lua/plugins/origami.lua
@@ -1,10 +1,17 @@
 -- lazy.nvim
 return {
-	"chrisgrieser/nvim-origami",
-	event = "VeryLazy",
-	opts = { foldKeymaps = { setup = true } },
-	init = function()
-		vim.opt.foldlevel = 99
-		vim.opt.foldlevelstart = 99
-	end,
+    'chrisgrieser/nvim-origami',
+    event = 'VeryLazy',
+    opts = {
+        foldKeymaps = { setup = false },
+        autoFold = {
+            enabled = true,
+            kinds = { 'imports' }, ---@type lsp.FoldingRangeKind[]
+        },
+
+    },
+    init = function()
+        vim.opt.foldlevel = 99
+        vim.opt.foldlevelstart = 99
+    end,
 }
