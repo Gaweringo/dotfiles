f6a617393f18eaa9198feea52ab40e5edfcb3d74
Gaweringo <40121865+Gaweringo@users.noreply.github.com>
2026-04-06 21:08:22 +0200

    refactor(nvim/tabterm): Make :TTerm tabterm command

--- config/nvim/plugin/tabterm.lua
+++ config/nvim/plugin/tabterm.lua
@@ -1,20 +1,14 @@
-local M = {}
-
-local function openTabterm()
-    vim.cmd.tabnew()
-    vim.cmd.terminal()
-    vim.cmd.startinsert({ bang = true })
-end
-
 -- TODO: Use TabEnter autocommand to track last tab page and make :Tabterm behave
 -- like a toggle to that tabpage and back to the last one
 -- vim.api.nvim_get_current_tabpage()
 -- :h tabpage.txt
 -- Make it possible to open multible tab terms
 
-vim.api.nvim_create_user_command('Tabterm', function()
-    openTabterm()
-end, {})
+vim.api.nvim_create_user_command('TTerm', function()
+    vim.cmd.tabnew()
+    vim.cmd.terminal()
+    vim.cmd.startinsert({ bang = true })
+end, { desc = 'Terminal in new Tab' })
 
 
 vim.api.nvim_create_user_command('VTerm', function()
@@ -23,5 +17,3 @@ vim.api.nvim_create_user_command('VTerm', function()
     vim.cmd.startinsert({ bang = true })
 end, { desc = 'Vertical split terminal' })
 
--- TODO: Create window pick and place plugin, so that you can press a keybind to pick up a window and press it again on
--- a different tabpage to drop it there (or maybe just use buffer search from Snacks picker)
