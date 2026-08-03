f3d48f457ad50df9f0543e3ead05ff15d9d8ba6a
Gaweringo <40121865+Gaweringo@users.noreply.github.com>
2026-08-03 16:24:48 +0200

    feat(nvim): add pubspec assist for flutter

--- config/nvim/lua/plugins/flutter.lua
+++ config/nvim/lua/plugins/flutter.lua
@@ -1,6 +1,7 @@
 local fts = { 'dart', 'flutterToolsOutline' }
 
 return {
+  {
     'nvim-flutter/flutter-tools.nvim',
     ft = 'dart',
     dependencies = { 'nvim-lua/plenary.nvim' },
@@ -9,5 +10,26 @@ return {
         { '<localleader>d', '<cmd>FlutterDebug<cr>', desc = 'Debug', ft = fts },
         { '<localleader>D', '<cmd>FlutterDevices<cr>', desc = 'Devices', ft = fts },
     },
+    opts = {
+      widget_guides = {
+        enabled = true,
+      },
+    }
+  },
+  {
+    'akinsho/pubspec-assist.nvim',
+    dependencies = { 'plenary.nvim' },
+    event = 'BufEnter pubspec.yaml',
+    init = function(_)
+      vim.api.nvim_create_autocmd({'BufEnter', 'BufCreate'}, {
+        pattern = 'pubspec.yaml',
+        callback = function(ev)
+          vim.keymap.set('n', '<localleader>a', '<cmd>PubspecAssistAddPackage<cr>', { desc = 'add package', buf = ev.buf })
+          vim.keymap.set('n', '<localleader>A', '<cmd>PubspecAssistAddDevPackage<cr>',{ desc = 'Add dev package', buf = ev.buf })
+          vim.keymap.set('n', '<localleader>v', '<cmd>PubspecAssistPickVersion<cr>', { desc = 'pick version', buf = ev.buf })
+        end,
+      })
+    end,
     config = true,
+  }
 }
